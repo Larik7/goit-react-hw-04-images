76101079e98a6577a2a947fe7cf33b2fa2221a3e
Larik7 <larik777taganka@gmail.com>
2023-03-30 15:05:34 +0300

    sixth commit

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -7,7 +7,6 @@ import { LoadMore } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 
 export const ImageGallery = ({ value }) => {
-  
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
@@ -19,19 +18,21 @@ export const ImageGallery = ({ value }) => {
       return;
     }
 
-    const fetchImages = async (value, page) => {
+    const fetchImages = async (query, currentPage) => {
       try {
         setLoading(true);
-        const resp = await fetchApi(value, page);
-  
+        const resp = await fetchApi(query, currentPage);
+
         if (!resp || resp.hits.length === 0) {
           setError(
             'Sorry, there are no images matching your search query. Please try again.'
           );
           return;
         }
-  
-        setImages([...images, ...resp.hits]);
+
+        setImages(prevState =>
+          page === 1 ? [...resp.hits] : [...prevState, ...resp.hits]
+        );
         setError(null);
         setHits(resp.totalHits);
       } catch (err) {
@@ -41,24 +42,13 @@ export const ImageGallery = ({ value }) => {
       }
     };
 
-    // const fetchImages = async () => {
-    //   try {
-    //     setLoading(true);
-    //     const resp = await fetchApi();
-    //   } catch (error) {
-    //     setError(String(error));
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // }
-
     fetchImages(value.trim(), page);
-  },[images, page, value]);
+  }, [value, page]);
 
   useEffect(() => {
     setImages([]);
     setPage(1);
-  },[value]);
+  }, [value]);
 
   const loadMore = () => {
     setPage(prevState => prevState + 1);
@@ -87,65 +77,3 @@ export const ImageGallery = ({ value }) => {
 ImageGallery.propTypes = {
   value: PropTypes.string.isRequired,
 };
-
-// componentDidUpdate(prevProps, prevState) {
-//   if (prevProps.value !== this.props.value) {
-//     this.setState({ images: [], page: 1 });
-//     this.fetchImages();
-//   }
-
-//   if (prevState.page < this.state.page) {
-//     this.fetchImages();
-//   }
-// }
-
-// fetchImages = async () => {
-//   try {
-//     this.setState({ loading: true });
-//     const resp = await fetchApi(this.props.value.trim(), this.state.page);
-
-//     if (!resp || resp.hits.length === 0) {
-//       this.setState({
-//         error:
-//           'Sorry, there are no images matching your search query. Please try again.',
-//       });
-//       return;
-//     }
-
-//     this.setState({
-//       images: [...this.state.images, ...resp.hits],
-//       error: null,
-//     });
-//     this.setState({ hits: resp.totalHits });
-//   } catch (err) {
-//     this.setState({ error: String(err) });
-//   } finally {
-//     this.setState({ loading: false });
-//   }
-// };
-
-// loadMore = () => {
-//   this.setState(prevState => ({ page: prevState.page + 1 }));
-// };
-
-// render() {
-//   const PER_PAGE = 12;
-//   const { images, error, hits, loading } = this.state;
-
-//   return (
-//     <>
-//       {error && <ErrorMessage>{error}</ErrorMessage>}
-//       <GalleryList>
-//         {images.length > 0 &&
-//           images.map(image => (
-//             <ListItem key={image.id}>
-//               <GalleryItem image={image} />
-//             </ListItem>
-//           ))}
-//       </GalleryList>
-
-//       {PER_PAGE < hits && <LoadMore handleClick={this.loadMore} />}
-//       {loading && <Loader />}
-//     </>
-//   );
-// }
